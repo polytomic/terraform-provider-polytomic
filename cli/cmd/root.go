e39be41a2cbbe3a1340cd06f09597bb9014f8839
Jake Neyer <jake@polytomic.com>
2023-02-26 13:32:43 -0500

    Inject importer version information during releases

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -8,8 +8,14 @@ import (
 	"github.com/spf13/viper"
 )
 
-func init() {
-	// Root flags
+func newRootCmd(version string) *cobra.Command {
+
+	rootCmd := &cobra.Command{
+		Use:     "polytomic-importer",
+		Version: version,
+		Short:   "Polytomic importer is a CLI tool to import existing Polytomic resources into Terraform",
+	}
+
 	var apiKey, url string
 	rootCmd.PersistentFlags().StringVar(&apiKey, "api-key", "", "Polytomic API key")
 	viper.BindPFlag("api-key", rootCmd.PersistentFlags().Lookup("api-key"))
@@ -28,14 +34,12 @@ func init() {
 
 	// Hide completions
 	rootCmd.CompletionOptions.HiddenDefaultCmd = true
+
+	return rootCmd
 }
 
-var rootCmd = &cobra.Command{
-	Use:   "polytomic-importer",
-	Short: "Polytomic importer is a CLI tool to import existing Polytomic resources into Terraform",
-}
-
-func Execute() {
+func Execute(version string) {
+	rootCmd := newRootCmd(version)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
