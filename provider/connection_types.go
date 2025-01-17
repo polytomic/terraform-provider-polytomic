1615261eddab2413dc4dd2af81053661491a8c31
Nathan Yergler <nathan@polytomic.com>
2025-01-17 10:43:11 -0800

    Correct attribute mismatch for Serverless conf

--- provider/connection_types.go
+++ provider/connection_types.go
@@ -13,7 +13,7 @@ type RedshiftServerlessConnectionConfiguration struct {
 	ExternalID string `json:"external_id,omitempty" mapstructure:"external_id" tfsdk:"external_id"`
 
 	ConnectionMethod   string `json:"connection_method,omitempty" mapstructure:"connection_method" tfsdk:"connection_method"`
-	ServerlessEndpoint string `json:"serverless_endpoint,omitempty" mapstructure:"serverless_endpoint" tfsdk:"serverless_endpoint"`
+	ServerlessEndpoint string `json:"endpoint,omitempty" mapstructure:"endpoint" tfsdk:"serverless_endpoint"`
 	OverrideEndpoint   bool   `json:"override_endpoint,omitempty" mapstructure:"override_endpoint" tfsdk:"override_endpoint"`
 	DataAPIEndpoint    string `json:"data_api_endpoint,omitempty" mapstructure:"data_api_endpoint" tfsdk:"data_api_endpoint"`
 }
