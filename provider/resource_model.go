9f8ca489097db2a77c21728f3f6fed7e964e9bba
Nathan Yergler <nathan@polytomic.com>
2025-08-22 07:58:10 -0700

    Tidying

--- provider/resource_model.go
+++ provider/resource_model.go
@@ -130,21 +130,6 @@ func (r *modelResource) Schema(ctx context.Context, req resource.SchemaRequest, 
 	}
 }
 
-type modelResourceResourceDataV0 struct {
-	ID               types.String `tfsdk:"id"`
-	Organization     types.String `tfsdk:"organization"`
-	Name             types.String `tfsdk:"name"`
-	Type             types.String `tfsdk:"type"`
-	Version          types.Int64  `tfsdk:"version"`
-	ConnectionID     types.String `tfsdk:"connection_id"`
-	Configuration    types.Map    `tfsdk:"configuration"`
-	Fields           types.Set    `tfsdk:"fields"`
-	AdditionalFields types.Set    `tfsdk:"additional_fields"`
-	Relations        types.Set    `tfsdk:"relations"`
-	Identifier       types.String `tfsdk:"identifier"`
-	TrackingColumns  types.Set    `tfsdk:"tracking_columns"`
-}
-
 type modelResourceResourceData struct {
 	ID               types.String `tfsdk:"id"`
 	Organization     types.String `tfsdk:"organization"`
@@ -781,100 +766,7 @@ func (r *modelResource) UpgradeState(ctx context.Context) map[int64]resource.Sta
 	return map[int64]resource.StateUpgrader{
 		// State upgrade implementation from 0 (prior state version) to 1 (Schema.Version)
 		0: {
-			PriorSchema: &schema.Schema{
-				Attributes: map[string]schema.Attribute{
-					"id": schema.StringAttribute{
-						MarkdownDescription: "",
-						Computed:            true,
-						PlanModifiers: []planmodifier.String{
-							stringplanmodifier.UseStateForUnknown(),
-						},
-					},
-					"organization": schema.StringAttribute{
-						MarkdownDescription: "",
-						Optional:            true,
-						Computed:            true,
-					},
-					"connection_id": schema.StringAttribute{
-						MarkdownDescription: "",
-						Required:            true,
-					},
-					"name": schema.StringAttribute{
-						MarkdownDescription: "",
-						Required:            true,
-					},
-					"type": schema.StringAttribute{
-						MarkdownDescription: "",
-						Computed:            true,
-						PlanModifiers: []planmodifier.String{
-							stringplanmodifier.UseStateForUnknown(),
-						},
-					},
-					"version": schema.Int64Attribute{
-						MarkdownDescription: "",
-						Computed:            true,
-						PlanModifiers: []planmodifier.Int64{
-							int64planmodifier.UseStateForUnknown(),
-						},
-					},
-					"configuration": schema.MapAttribute{
-						MarkdownDescription: "",
-						ElementType:         types.StringType,
-						Required:            true,
-					},
-					"fields": schema.SetAttribute{
-						MarkdownDescription: "",
-						ElementType:         types.StringType,
-						Optional:            true,
-						Computed:            true,
-					},
-					"additional_fields": schema.SetAttribute{
-						MarkdownDescription: "",
-						ElementType: types.ObjectType{
-							AttrTypes: map[string]attr.Type{
-								"name":  types.StringType,
-								"type":  types.StringType,
-								"label": types.StringType,
-							},
-						},
-						Optional: true,
-						Computed: true,
-					},
-					"relations": schema.SetAttribute{
-						MarkdownDescription: "",
-						ElementType: types.ObjectType{
-							AttrTypes: map[string]attr.Type{
-								"to": types.ObjectType{
-									AttrTypes: map[string]attr.Type{
-										"model_id": types.StringType,
-										"field":    types.StringType,
-									},
-								},
-								"from": types.StringType,
-							},
-						},
-						Optional: true,
-						Computed: true,
-					},
-					"identifier": schema.StringAttribute{
-						MarkdownDescription: "",
-						Optional:            true,
-						Computed:            true,
-						PlanModifiers: []planmodifier.String{
-							stringplanmodifier.UseStateForUnknown(),
-						},
-					},
-					"tracking_columns": schema.SetAttribute{
-						MarkdownDescription: "",
-						ElementType:         types.StringType,
-						Optional:            true,
-						Computed:            true,
-						PlanModifiers: []planmodifier.Set{
-							setplanmodifier.UseStateForUnknown(),
-						},
-					},
-				},
-			},
+			PriorSchema: v0ModelSchema,
 			StateUpgrader: func(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
 				var priorStateData modelResourceResourceDataV0
 
